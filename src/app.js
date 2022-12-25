ae82756f3b10eb436ef88905756d90c5e791ebe4
NinaKlim <nnikitka@gmail.com>
2022-12-25 19:41:28 +0100

    geocoding by city name, get daily forecast

--- src/app.js
+++ src/app.js
@@ -10,6 +10,35 @@ dayTime.forEach(function (time) {
    <div>+22</div>
    </div> `;
 });
-
 forecastHTML = forecastHTML + `</div>`;
 forecastElements.innerHTML = forecastHTML;
+
+function showForecast(response) {
+  console.log(response);
+}
+
+function showWeather(response) {
+  let lat = response.data[0].lat;
+  let lon = response.data[0].lon;
+  let apiKey = `017d56650cd168d68067850318775d43`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+
+  // console.log(response);
+  axios.get(apiUrl).then(showForecast);
+}
+
+function getLocation(cityValue) {
+  let apiKey = `017d56650cd168d68067850318775d43`;
+  let apiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityValue}&limit=1&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+function getCity(event) {
+  event.preventDefault();
+  let cityValue = document.querySelector("#cityInput").value;
+  getLocation(cityValue);
+}
+
+document.querySelector("#searchCityForm").addEventListener("submit", getCity);
+getLocation("Kyiv");
