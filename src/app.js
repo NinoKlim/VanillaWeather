4fc227b946a3fa053ba20a1bdbb8d98853da55ee
NinaKlim <nnikitka@gmail.com>
2022-12-29 22:12:36 +0100

    catch() method from mdn

--- src/app.js
+++ src/app.js
@@ -62,14 +62,22 @@ function curCity(response) {
   let apiKey = `017d56650cd168d68067850318775d43`;
   let apiUrl = `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
   axios.get(apiUrl).then(displayCity);
-  promise.then(
-    function () {
-      doSthHere();
-    },
-    function () {}
-  );
 }
+const p1 = new Promise((resolve, reject) => {
+  resolve("Success");
+});
 
+p1.then((value) => {
+  console.log(value); // "Success!"
+  throw new Error("oh, no!");
+})
+  .catch((e) => {
+    console.error(e.message); // "oh, no!"
+  })
+  .then(
+    () => console.log("after a catch the chain is restored"),
+    () => console.log("Not fired due to the catch")
+  );
 navigator.geolocation.getCurrentPosition(curCity);
 
 let celsiusLink = document.querySelector("#celcius");
