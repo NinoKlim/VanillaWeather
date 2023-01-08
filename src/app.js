6d40882b673f9d46563be9afceb8b4f01731aa0e
NinaKlim <nnikitka@gmail.com>
2023-01-08 19:57:51 +0100

    del fahr, remove navigator, add IPgeoloc, clean

--- src/app.js
+++ src/app.js
@@ -63,19 +63,15 @@ function showForecast(response) {
     "src",
     `https://github.com/NinoKlim/VanillaWeather/blob/main/media/${response.data.weather[0].icon}.png?raw=true`
   );
-  document.querySelector("#date").innerHTML = curDate(
-    response.data.dt * 1000 - response.data.timezone * 1000
-  );
+  document.querySelector("#date").innerHTML = curDate(response.data.dt * 1000);
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
   );
   document.querySelector("#hum").innerHTML = response.data.main.humidity;
-  console.log(response.data);
   getDailyForecast(response.data.coord);
 }
 
 function getDailyForecast(coordinates) {
-  console.log(coordinates);
   let apiKey = `017d56650cd168d68067850318775d43`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showDailyForecast);
@@ -95,36 +91,9 @@ function getLocation(cityValue) {
 }
 
 function currentCity(position) {
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiKey = `017d56650cd168d68067850318775d43`;
-  let apiUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`;
-  axios.get(apiUrl).then(displayCity);
+  getLocation(position.data.city);
 }
 
-let celsiusLink = document.querySelector("#celcius");
-celsiusLink.addEventListener("click", showCelcius);
-
-let fahrenheitLink = document.querySelector("#fahrenheit");
-fahrenheitLink.addEventListener("click", showFahrenheit);
-
-function displayCity(cityValue) {
-  getLocation(cityValue.data[0].name);
-}
-
-navigator.geolocation.getCurrentPosition(currentCity);
-
-function showCelcius(event) {
-  event.preventDefault();
-  celsiusLink.classList.add("unitActive");
-  fahrenheitLink.classList.remove("unitActive");
-  document.querySelector("#temperature").innerHTML = celsiusTemperature;
-}
-
-function showFahrenheit(event) {
-  event.preventDefault();
-  celsiusLink.classList.remove("unitActive");
-  fahrenheitLink.classList.add("unitActive");
-  let fahrenheitTemp = (celsiusTemperature * 9) / 5 + 32;
-  document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemp);
-}
+let token = `cd734928f9329a`;
+let apiUrl = `https://ipinfo.io/json?token=${token}`;
+axios.get(apiUrl).then(currentCity);
